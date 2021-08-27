822cafa2c22762660ee2e6cc8a58b0c2f8c50f6e
Naman-HotWax <naman.garg@hotwaxsystems.com>
2021-08-28 01:17:48 +0530

    Implemented: Open & Packed segment in Orders tab (#7q6u5d)

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -4,6 +4,19 @@
       <ion-toolbar>
         <ion-title>{{ $t("Orders") }}</ion-title>
       </ion-toolbar>
+      <ion-toolbar>
+        <ion-searchbar placeholder="Search orders"></ion-searchbar>
+      </ion-toolbar>
+      <ion-toolbar>
+        <ion-segment @ionChange="segmentChanged($event)" v-model="segment">
+          <ion-segment-button value="orders">
+            <ion-label>OPEN</ion-label></ion-segment-button
+          >
+          <ion-segment-button value="packedorders">
+            <ion-label>PACKED</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
@@ -13,17 +26,32 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonContent, IonHeader, IonLabel, IonPage, IonSearchbar, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'Orders',
   components: {
     IonContent,
     IonHeader,
+    IonLabel,
     IonPage,
+    IonSearchbar,
+    IonSegment,
+    IonSegmentButton,
     IonTitle,
     IonToolbar
+  },
+  methods: {
+    segmentChanged(ev: CustomEvent) {
+      this.segment = ev.detail.value;
+    },
+  },
+  setup() {
+    const segment = ref("orders");
+    return {
+      segment
+    };
   }
 });
 </script>
