fdc86cb656cb149aa7d8ffe4a81daf6afb144fdc
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2021-11-08 14:15:37 +0530

    Created the new branch and creating the static ui for order fullfillment app

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,9 +1,26 @@
 <template>
   <ion-page>
+
+    <!-- Designing the Header -->
+
     <ion-header :translucent="true">
+
+      <!-- First Line view -->
       <ion-toolbar>
         <ion-title>{{ $t("Orders") }}</ion-title>
+        <ion-buttons slot="end">
+          <ion-button>
+              <ion-icon :icon="swapVerticalOutline" />
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
+
+      <!-- Search bar view -->
+      <ion-toolbar>
+        <ion-searchbar  placeholder="Search Item"></ion-searchbar>
+      </ion-toolbar>
+
+      
     </ion-header>
     
     <ion-content :fullscreen="true">
@@ -15,6 +32,7 @@
 <script lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { swapVerticalOutline } from 'ionicons/icons'
 
 export default defineComponent({
   name: 'Orders',
@@ -24,6 +42,11 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonToolbar
+  },
+  setup(){
+    return {
+      swapVerticalOutline,
+    }
   }
 });
 </script>
