38f8876c41e03de6d9a1f17d1a3268030012eee7
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2021-11-08 15:17:09 +0530

    Created a new branch and started with the static ui of order fulfillment app

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -20,18 +20,37 @@
         <ion-searchbar  placeholder="Search Item"></ion-searchbar>
       </ion-toolbar>
 
-      
+      <!-- Segment part where we can switch between open and packed orders -->
+      <ion-segment @ionChange="segmentChanged($event)" v-model="segmentName"> 
+        <ion-segment-button value="open">
+          <ion-label>OPEN</ion-label>
+        </ion-segment-button>
+        <ion-segment-button value="packed">
+          <ion-label>PACKED</ion-label>
+        </ion-segment-button>
+      </ion-segment>
     </ion-header>
     
+
+    <!-- Content of the app -->
     <ion-content :fullscreen="true">
    
+      <!-- if employer is on the open segment -->
+      <div v-if="segmentName=='open'">
+        In this the open products will be shown
+      </div>
+
+      <!-- if employer is on the packed segment -->
+      <div v-if="segmentName=='packed'">
+        In this the packed segments will be shown
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSegment, IonSegmentButton, IonSearchbar} from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
 import { swapVerticalOutline } from 'ionicons/icons'
 
 export default defineComponent({
@@ -41,12 +60,28 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonSegmentButton,
+    IonSegment,
+    IonSearchbar
+  },
+  methods: {
+    segmentChanged(ev: CustomEvent) {
+      console.log('Segment changed', ev);
+    }
   },
   setup(){
+    const segmentName = ref("open")
     return {
       swapVerticalOutline,
+      segmentName
     }
   }
 });
 </script>
+
+<style scoped>
+ion-searchbar{
+  box-shadow: lightgreen;
+}
+</style>
