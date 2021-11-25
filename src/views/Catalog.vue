16bffcf48970b084275fe42789fae1bf2b85c153
Yashi002 <vermayashi002@gmail.com>
2021-11-25 16:24:42 +0530

    Used Ionic Grid

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -5,53 +5,61 @@
         <ion-title>{{ $t("Catalog") }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">    
-      <ion-searchbar />
-      <ion-row>
-        <ion-col>
-          <ion-card>            
-            <ion-card-header>
-              <Image :src="'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80'" />
-              <ion-label>
-                <h2 class="black-text" style="color: black;">Brand</h2>
-                <h2 class="black-text" style="color: black;">Parent Name</h2>
-                <p>{{ $t("$Sale price") }}</p>
+    <ion-content :fullscreen="true">  
+        
+      <ion-searchbar />                    
+        <div class="product-item">  
+          <ion-card>           
+          <ion-card-header>
+            <Image :src="'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80'" />
+              <ion-label color="dark">
+                <h2>Brand</h2>
+                <h2>Parent Name</h2>
+                <p>{{ $t("$")}} Sale price</p>
               </ion-label>
-            </ion-card-header>             
-          </ion-card>  
-        </ion-col>
-        <ion-col> 
+          </ion-card-header>
+          </ion-card>                  
           <ion-card>
-            <ion-card-header>
-              <Image :src="'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80'" />
-              <ion-label>
-                <h2 class="black-text" style="color: black;">Brand</h2>
-                <h2 class="black-text" style="color: black;">Parent Name</h2>
-                <p>{{ $t("$Sale price") }}</p>
+          <ion-card-header>
+            <Image :src="'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80'" />
+              <ion-label color="dark">
+                <h2>Brand</h2>
+                <h2>Parent Name</h2>
+                <p>{{ $t("$") }} Sale price</p>
               </ion-label>
-            </ion-card-header>            
-          </ion-card>  
-        </ion-col>    
-      </ion-row>      
+            </ion-card-header>  
+          </ion-card> 
+        </div>                
     </ion-content> 
   </ion-page>
 </template>
 
 <script lang="ts">
 import Image from "@/components/Image.vue";
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonContent, IonHeader, IonLabel, IonPage, IonSearchbar, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Catalog',
   components: {
     Image,
-    IonContent,
+    IonCard, 
+    IonCardHeader,   
+    IonContent, 
     IonHeader,
-    IonPage,
-    IonTitle,
+    IonLabel, 
+    IonPage, 
+    IonSearchbar, 
+    IonTitle, 
     IonToolbar
     
   }
-});
+}
+);
 </script>
+<style scoped>
+ .product-item {
+   display: grid;
+   grid-template-columns: repeat( auto-fill, minmax(150px, 1fr) );
+ }
+</style>
