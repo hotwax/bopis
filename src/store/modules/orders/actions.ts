45b9f4129c0ff1e59f5136bb0eff8c93d278e134
Yash Maheshwari <yashmaheshwari551@gmail.com>
2021-12-08 18:38:13 +0530

    Implemented: actions to ship and deliver the open and packed orders(#1w40vqa)

--- src/store/modules/orders/actions.ts
+++ src/store/modules/orders/actions.ts
@@ -13,6 +13,8 @@ const actions: ActionTree<OrdersState , RootState> ={
     if (payload.viewIndex === 0) emitter.emit("presentLoader");
     let resp;
 
+		console.log('getOrders')
+
     try {
 			resp = await OrderService.getOrders(payload)
 			if (resp.status === 200 && resp.data.count > 0 && !hasError(resp)) {
@@ -60,8 +62,9 @@ const actions: ActionTree<OrdersState , RootState> ={
   	return resp;
 	},
 
-	async deliverShipment ({ dispatch }, order: any) {
-		console.log(order);
+	async deliverShipment ({ dispatch }, order) {
+		emitter.emit("presentLoader");
+
 		const params = {
       shipmentId: order.shipmentId,
       statusId: 'SHIPMENT_SHIPPED'
@@ -72,25 +75,78 @@ const actions: ActionTree<OrdersState , RootState> ={
 		try {
 			resp = await OrderService.updateShipment(params)
 			if (resp.status === 200 && !hasError(resp)) {
-				console.log('shippend')
-				// let packedOrders = resp.data.docs;
-				// const ordersCount = resp.data.count;
-				// if(payload.viewIndex && payload.viewIndex > 0) packedOrders = state.packedOrders.concat(packedOrders)
-				// commit(types.ORDERS_PACKED_INITIAL, { packedOrders })
+				showToast(translate(`Order delivered to ${order.customerName}`))
+				emitter.emit("dismissLoader");
 			} else {
-				showToast(translate("Orders Not Found"))
+				emitter.emit("dismissLoader");
+				showToast(translate("Something went wrong"))
 			}
-			// if (payload.viewIndex === 0) emitter.emit("dismissLoader");
     } catch(err) {
 			console.log(err)
+			emitter.emit("dismissLoader");
 			showToast(translate("Something went wrong"))
     }
 
   	return resp;
 	},
 
-	quickShipEntireShipGroup ({ commit }) {
-		console.log('ship group action')
+	getShipmentMethod({ commit }, payload) {
+    /* To display the button label as per the shipmentMethodTypeId, this will only used on orders segment.
+        Because we get the shipmentMethodTypeId on items level in wms-orders API.
+        As we already get shipmentMethodTypeId on order level in readytoshiporders API hence we will not use this method on packed orders segment.
+    */
+    return payload.items.find((ele: any) => ele.shipGroupSeqId == payload.shipGroupSeqId).shipmentMethodTypeId
+  },
+
+	async packDeliveryItems ({ commit }, shipmentId) {
+		const params = {
+      shipmentId: shipmentId,
+      statusId: 'SHIPMENT_PACKED'
+    }
+		return await OrderService.updateShipment(params)
+	},
+
+	async quickShipEntireShipGroup ({ dispatch }, payload) {
+		emitter.emit("presentLoader")
+
+		const params = {
+      orderId: payload.order.orderId,
+      setPackedOnly: 'Y',
+      dimensionUomId: 'WT_kg',
+      shipmentBoxTypeId: 'YOURPACKNG',
+      weight: '1',
+      weightUomId: 'WT_kg',
+      facilityId: payload.facilityId,
+      shipGroupSeqId: payload.shipGroup
+    }
+		
+		let resp;
+
+		try {
+			resp = await OrderService.quickShipEntireShipGroup(params)
+			console.log(resp)
+			if (resp.status === 200 && !hasError(resp) && resp.data._EVENT_MESSAGE_) {
+				const shipmentMethod = await dispatch('getShipmentMethod', {shipGroupSeqId: payload.shipGroup, items: payload.order.items})
+				console.log(shipmentMethod);
+				if (shipmentMethod !== 'STOREPICKUP') {
+					const shipmentId = resp.data._EVENT_MESSAGE_.match(/\d+/g)[0]
+					dispatch('packDeliveryItems', shipmentId).then((data) => {
+						console.log(data)
+						if (!hasError(data) && !data.data._EVENT_MESSAGE_) showToast(translate("Something went wrong"))
+					})
+				}
+				emitter.emit("dismissLoader");
+				showToast(translate("Order packed and ready for delivery"))
+			} else {
+				console.log('else')
+				emitter.emit("dismissLoader");
+				showToast(translate("Something went wrong"))
+			}
+    } catch(err) {
+			console.log(err)
+			emitter.emit("dismissLoader");
+			showToast(translate("Something went wrong"))
+    }
 	}
 }
 
