e316c7b0dc07ee0b36b198a80681ad4bb5778a02
Yash Maheshwari <yashmaheshwari551@gmail.com>
2021-12-09 16:58:25 +0530

    Used: ion-page component to solve the issue of page not rendering(#1w40vqa)

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -1,65 +1,67 @@
 <template>
-  <ion-header>
-    <ion-toolbar>
-      <ion-back-button default-href="/" slot="start" />
-      <ion-title>{{ $t("Order details") }}</ion-title>
-    </ion-toolbar>
-  </ion-header>
-  <ion-content>
-    <ion-list>
-      <ion-item lines="none">
-        <ion-label>
-          <h2>{{ orders.customerName }}</h2>
-          <p>{{ orders.orderId }}</p>
-        </ion-label>
-        <ion-note slot="end">{{ $filters.formatDate(orders.orderDate) }}</ion-note>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-back-button default-href="/" slot="start" />
+        <ion-title>{{ $t("Order details") }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item lines="none">
+          <ion-label>
+            <h2>{{ orders.customerName }}</h2>
+            <p>{{ orders.orderId }}</p>
+          </ion-label>
+          <ion-note slot="end">{{ $filters.formatDate(orders.orderDate) }}</ion-note>
+        </ion-item>
+      </ion-list>
+      <ion-item v-if="orders.phoneNumber">
+        <ion-icon :icon="callOutline" slot="start" />
+        <ion-label>{{ orders.phoneNumber }}</ion-label>
+        <ion-button
+          fill="outline"
+          slot="end"
+          color="medium"
+          @click="copyToClipboard(orders.phoneNumber)"
+        >
+          {{ $t("Copy") }}
+        </ion-button>
       </ion-item>
-    </ion-list>
-    <ion-item v-if="orders.phoneNumber">
-      <ion-icon :icon="callOutline" slot="start" />
-      <ion-label>{{ orders.phoneNumber }}</ion-label>
-      <ion-button
-        fill="outline"
-        slot="end"
-        color="medium"
-        @click="copyToClipboard(orders.phoneNumber)"
-      >
-        {{ $t("Copy") }}
-      </ion-button>
-    </ion-item>
-    <ion-item v-if="orders.email" lines="none">
-      <ion-icon :icon="mailOutline" slot="start" />
-      <ion-label>{{ orders.email }}</ion-label>
-      <ion-button
-        fill="outline"
-        slot="end"
-        color="medium"
-        @click="copyToClipboard(orders.email)"
-      >
-        {{ $t("Copy") }}
-      </ion-button>
-    </ion-item>
+      <ion-item v-if="orders.email" lines="none">
+        <ion-icon :icon="mailOutline" slot="start" />
+        <ion-label>{{ orders.email }}</ion-label>
+        <ion-button
+          fill="outline"
+          slot="end"
+          color="medium"
+          @click="copyToClipboard(orders.email)"
+        >
+          {{ $t("Copy") }}
+        </ion-button>
+      </ion-item>
 
-    <ion-card v-for="(item, index) in orders?.items" :key="index">
-      <ion-card-content>
-        <ProductListItem :item="item" />
-      </ion-card-content>
-    </ion-card>
-    <ion-item lines="none">
-      <ion-note>
-        {{ $t(`This order cannot be split. If you cannot fulfill any item, ${orders.customerName} will be sent an email with alternate fulfillment options and this order will be removed from your dashboard.`) }}
-      </ion-note>
-    </ion-item>
-    <ion-button expand="block" color="danger" fill="outline" @click="updateOrder(orders)">
-      {{ $t("Reject Order") }}
-    </ion-button>
-    <ion-card>
+      <ion-card v-for="(item, index) in orders?.items" :key="index">
+        <ion-card-content>
+          <ProductListItem :item="item" />
+        </ion-card-content>
+      </ion-card>
       <ion-item lines="none">
-        <ion-icon :icon="informationCircleOutline" slot="start" @click="learnMore"/>
-        <ion-label>{{ $t("Learn more about unfillable items") }}</ion-label>
+        <ion-note>
+          {{ $t(`This order cannot be split. If you cannot fulfill any item, will be sent an email with alternate fulfillment options and this order will be removed from your dashboard.`, { customerName: orders.customerName }) }}
+        </ion-note>
       </ion-item>
-    </ion-card>
-  </ion-content>
+      <ion-button expand="block" color="danger" fill="outline" @click="updateOrder(orders)">
+        {{ $t("Reject Order") }}
+      </ion-button>
+      <ion-card>
+        <ion-item lines="none">
+          <ion-icon :icon="informationCircleOutline" slot="start" @click="learnMore"/>
+          <ion-label>{{ $t("Learn more about unfillable items") }}</ion-label>
+        </ion-item>
+      </ion-card>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
@@ -73,6 +75,7 @@ import {
   IonHeader,
   IonIcon,
   IonItem,
+  IonPage,
   IonLabel,
   IonList,
   IonNote,
@@ -105,6 +108,7 @@ export default defineComponent({
     IonHeader,
     IonIcon,
     IonItem,
+    IonPage,
     IonLabel,
     IonList,
     IonNote,
