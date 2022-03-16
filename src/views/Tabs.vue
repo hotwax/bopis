3ed3a6474cd41d29431242bf40a44f3ff7be0a25
azkyakhan <azkya.khan@hotwax.co>
2022-03-16 15:34:26 +0530

    Removed margin(#23ecz5d)

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -50,7 +50,6 @@ ion-tab-bar {
   bottom: 5%;
   box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
   width: 375px;
-  margin: auto;
   border-radius: 15px;
 }
 </style>
