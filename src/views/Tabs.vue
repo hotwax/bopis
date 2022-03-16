79de2a1e66ec80df5b790ed3bc7fb901c8bc80a9
azkyakhan <azkya.khan@hotwax.co>
2022-03-16 15:27:17 +0530

    Improved styling of ion-tab-bar(#23ecz5d)

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -44,7 +44,9 @@ export default {
 </script>
 <style scoped>
 ion-tab-bar {
-  position: relative;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
   bottom: 5%;
   box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
   width: 375px;
