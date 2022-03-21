8fd9c4efb1c32c473c648571f09c58bfd98278c4
Aditya Patel <aditya.patel@hotwax.co>
2022-03-21 18:33:58 +0530

    Update Tabs.vue
    used spacer

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -54,7 +54,7 @@ ion-tab-bar {
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
-    bottom: 24px;
+    bottom: var(--spacer-base);
     width: 375px;
     transition: width .5s ease-in-out, bottom 1s ease-in-out;
     box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px; 
