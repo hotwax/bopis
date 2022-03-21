ae4d6e4bb494914a3fff2620ccb9a90be2158e6e
azkyakhan <azkya.khan@hotwax.co>
2022-03-21 18:12:47 +0530

    Improved styling on ion-tab-bar by applying transition animation (#23zmgmg)

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -44,14 +44,20 @@ export default {
 </script>
 
 <style scoped>
+ion-tab-bar { 
+  bottom: 0px;
+  width: 100%;
+}
+
 @media (min-width: 991px) {
   ion-tab-bar {
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
     bottom: 24px;
-    box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
     width: 375px;
+    transition: width .5s ease-in-out, bottom 1s ease-in-out;
+    box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px; 
     border-radius: 15px;
   }
 }
