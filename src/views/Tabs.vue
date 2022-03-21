cd3650ddd7534a7ecba246e34b575cbdfab28f8e
Aditya Patel <aditya.patel@hotwax.co>
2022-03-21 18:36:09 +0530

    Update Tabs.vue
    Moved transition to base rule so it applies both ways

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -47,6 +47,7 @@ export default {
 ion-tab-bar { 
   bottom: 0px;
   width: 100%;
+  transition: width .5s ease-in-out, bottom 1s ease-in-out;
 }
 
 @media (min-width: 991px) {
@@ -56,7 +57,6 @@ ion-tab-bar {
     transform: translateX(-50%);
     bottom: var(--spacer-base);
     width: 375px;
-    transition: width .5s ease-in-out, bottom 1s ease-in-out;
     box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px; 
     border-radius: 15px;
   }
