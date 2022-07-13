ca0a079cb2cbb5ee84a0625267cdb8cf7baa10f7
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-07-13 16:55:46 +0530

    Implemented: support to set and clear the token and instanceUrl for the oms pacakge(#2epx5vr)

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { loadingController } from '@ionic/vue';
 import emitter from "@/event-bus"
-
+import { mapGetters } from 'vuex';
+import { updateToken, updateInstanceUrl } from '@hotwax/oms-api/api'
 
 export default defineComponent({
   name: 'App',
@@ -53,10 +54,20 @@ export default defineComponent({
       });
     emitter.on('presentLoader', this.presentLoader);
     emitter.on('dismissLoader', this.dismissLoader);
+    updateToken(this.userToken)
+    updateInstanceUrl(this.instanceUrl)
   },
   unmounted() {
     emitter.off('presentLoader', this.presentLoader);
     emitter.off('dismissLoader', this.dismissLoader);
+    updateToken('')
+    updateInstanceUrl('')
   },
+  computed: {
+    ...mapGetters({
+      userToken: 'user/getUserToken',
+      instanceUrl: 'user/getInstanceUrl'
+    })
+  }
 });
 </script>
