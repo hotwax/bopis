98c6f975a4aec7cb55d433f363820ccfdc68b40b
shashwatbangar <shashwatbangar@gmail.com>
2022-12-07 12:35:17 +0530

    Implemented: Updated UI of Settings page(#32j3r6t)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -7,67 +7,124 @@
     </ion-header>
     
     <ion-content>
-      <ion-list>
-        <!-- Select store -->
-        <ion-item>
-          <ion-icon :icon="storefrontOutline" slot="start" />
-          <ion-label>{{$t("Store")}}</ion-label>
-          <ion-select interface="popover" :value="currentFacility.facilityId" @ionChange="setFacility($event)">
-            <ion-select-option v-for="facility in (userProfile ? userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.name }}</ion-select-option>
-          </ion-select>
-        </ion-item>
-        <!-- Shipping order preference -->
-        <ion-item>
-          <ion-icon :icon="sendOutline" slot="start" />
-          <ion-label>{{$t("Shipping orders")}}</ion-label>
-          <ion-toggle :checked="showShippingOrders" @ionChange="setShowShippingOrdersPreference($event)" slot="end" />
-        </ion-item>
-        <!-- Packing document preference -->
-        <ion-item>
-          <ion-icon :icon="sendOutline" slot="start" />
-          <ion-label>{{ $t("Packing slip") }}</ion-label>
-          <ion-toggle :checked="showPackingSlip" @ionChange="setShowPackingSlipPreference($event)" slot="end" />
-        </ion-item>
-        <!-- OMS information -->
-        <ion-item>
-          <ion-icon :icon="codeWorkingOutline" slot="start"/>
-          <ion-label>{{ $t("OMS") }}</ion-label>
-          <p slot="end">{{ baseURL ? baseURL : instanceUrl }}</p>
-        </ion-item>
-        <!-- Profile of user logged in -->
-        <ion-item>
-          <ion-icon :icon="personCircleOutline" slot="start" />
-          <ion-label>{{ userProfile !== null ? userProfile.partyName : '' }}</ion-label>
-          <ion-button slot="end" fill="outline" color="dark" @click="logout()">{{ $t("Logout") }}</ion-button>
-        </ion-item>
-      </ion-list>
+      <div class="user-profile">
+        <ion-card>
+          <ion-item lines="full">
+            <ion-avatar slot="start" v-if="userProfile?.partyImageUrl">
+              <Image :src="userProfile.partyImageUrl"/>
+            </ion-avatar>
+            <ion-label>
+              {{ userProfile?.partyName }}
+              <p>{{ userProfile?.userLoginId }}</p>
+            </ion-label>
+          </ion-item>
+          <ion-button fill="outline" color="danger" @click="logout()">{{ $t("Logout") }}</ion-button>
+          <!-- Commenting this code as we currently do not have reset password functionality -->
+          <!-- <ion-button fill="outline" color="medium">{{ $t("Reset password") }}</ion-button> -->
+        </ion-card>
+      </div>
+      <h1>{{ $t('OMS') }}</h1>
+      <section>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>
+              {{ $t("OMS instance") }}
+            </ion-card-subtitle>
+            <ion-card-title>
+              {{ instanceUrl }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            {{ $t('This is the name of the OMS you are connected to right now. Make sure that you are connected to the right instance before proceeding.') }}
+          </ion-card-content>
+          <ion-button @click="goToOms" fill="clear">
+            {{ $t('Go to OMS') }}
+            <ion-icon slot="end" :icon="openOutline" />
+          </ion-button>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>
+              {{ $t("Shopify Config") }}
+            </ion-card-subtitle>
+            <ion-card-title>
+              {{ $t("eCommerce") }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            {{ $t('eCommerce stores are directly connected to one Shop Configs. If your OMS is connected to multiple eCommerce stores selling the same catalog operating as one Company, you may have multiple Shop Configs for the selected Product Store.') }}
+          </ion-card-content>
+          <ion-item lines="none">
+            <ion-label>{{ $t("Select eCommerce") }}</ion-label>
+            <ion-select interface="popover" :value="currentFacility.facilityId" @ionChange="setFacility($event)">
+              <ion-select-option v-for="facility in (userProfile ? userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.name }}</ion-select-option>
+            </ion-select>
+          </ion-item>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>
+              {{ $t("Shipping orders") }}
+            </ion-card-subtitle>
+            <ion-card-title>
+              {{ $t("Shipping orders") }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-item lines="none">
+            <ion-toggle :checked="showShippingOrders" @ionChange="setShowShippingOrdersPreference($event)" slot="end" />
+          </ion-item>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>
+              {{ $t("Packing slip") }}
+            </ion-card-subtitle>
+            <ion-card-title>
+              {{ $t("Packing slip") }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-item lines="none">
+            <ion-toggle :checked="showPackingSlip" @ionChange="setShowPackingSlipPreference($event)" slot="end" />
+          </ion-item>
+        </ion-card>
+      </section>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonSelect, IonSelectOption, IonTitle, IonToggle , IonToolbar } from '@ionic/vue';
+import { IonAvatar, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToggle , IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { ellipsisVertical, personCircleOutline, sendOutline , storefrontOutline, codeWorkingOutline } from 'ionicons/icons'
+import { ellipsisVertical, personCircleOutline, sendOutline , storefrontOutline, codeWorkingOutline, openOutline } from 'ionicons/icons'
 import { mapGetters, useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+import Image from '@/components/Image.vue';
 
 export default defineComponent({
   name: 'Settings',
   components: {
+    IonAvatar,
     IonButton, 
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
     IonContent, 
     IonHeader, 
     IonIcon,
     IonItem, 
     IonLabel, 
-    IonList,
     IonPage, 
     IonSelect, 
     IonSelectOption,
     IonTitle,
     IonToggle, 
-    IonToolbar
+    IonToolbar,
+    Image
   },
   data(){
     return {
@@ -100,6 +157,16 @@ export default defineComponent({
     },
     setShowPackingSlipPreference (ev: any){
       this.store.dispatch('user/setUserPreference', { showPackingSlip: ev.detail.checked })
+    },
+    setEComStore(store: any) {
+      if(this.userProfile) {
+        this.store.dispatch('user/setEcomStore', {
+          'eComStore': this.userProfile.stores.find((str: any) => str.productStoreId == store['detail'].value)
+        })
+      }
+    },
+    goToOms(){
+      window.location.href = this.instanceUrl.startsWith('http') ? this.instanceUrl.replace('api/', "") : `https://${this.instanceUrl}.hotwax.io/`;
     }
   },
   setup () {
@@ -113,8 +180,30 @@ export default defineComponent({
       sendOutline,
       store,
       storefrontOutline,
-      codeWorkingOutline
+      codeWorkingOutline,
+      openOutline
     }
   }
 });
 </script>
+
+<style scoped>
+  ion-card > ion-button {
+    margin: var(--spacer-xs);
+  }
+  h1 {
+    padding: var(--spacer-xs) 10px 0;
+  }
+  section {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    align-items: start;
+  }
+  .user-profile {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  }
+  hr {
+    border-top: 1px solid var(--ion-color-medium);
+  }
+</style>
