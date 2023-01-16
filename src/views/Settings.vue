d4d529d7f5cfa841970e74d60e60854638477cac
shashwatbangar <shashwatbangar@gmail.com>
2023-01-16 13:16:50 +0530

    Implemented: Code to show app version & build information on Settings page(#85zrhn8w8)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -13,17 +13,19 @@
             <ion-avatar slot="start" v-if="userProfile?.partyImageUrl">
               <Image :src="userProfile.partyImageUrl"/>
             </ion-avatar>
-            <ion-label>
-              {{ userProfile?.partyName }}
-              <p>{{ userProfile?.userLoginId }}</p>
-            </ion-label>
+            <ion-card-header>
+              <ion-card-subtitle>{{ userProfile?.userLoginId }}</ion-card-subtitle>
+              <ion-card-title>{{ userProfile?.partyName }}</ion-card-title>
+            </ion-card-header>
           </ion-item>
           <ion-button fill="outline" color="danger" @click="logout()">{{ $t("Logout") }}</ion-button>
           <!-- Commenting this code as we currently do not have reset password functionality -->
           <!-- <ion-button fill="outline" color="medium">{{ $t("Reset password") }}</ion-button> -->
         </ion-card>
       </div>
-      <h1>{{ $t('OMS') }}</h1>
+      <div class="section-header">
+        <h1>{{ $t('OMS') }}</h1>
+      </div>
       <section>
         <ion-card>
           <ion-card-header>
@@ -90,6 +92,16 @@
             <ion-toggle :checked="showPackingSlip" @ionChange="setShowPackingSlipPreference($event)" slot="end" />
           </ion-item>
         </ion-card>
+
+      <hr />
+
+      <div class="section-header">
+        <h1>
+          {{ $t('App') }}
+          <p class="overline" >{{ "Version: " + appVersion }}</p>
+        </h1>
+        <p class="overline">{{ "Built: " + getDateTime(appInfo.builtTime) }}</p>
+      </div>
       </section>
     </ion-content>
   </ion-page>
@@ -102,6 +114,7 @@ import { ellipsisVertical, personCircleOutline, sendOutline , storefrontOutline,
 import { mapGetters, useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import Image from '@/components/Image.vue';
+import { DateTime } from 'luxon';
 
 export default defineComponent({
   name: 'Settings',
@@ -128,7 +141,9 @@ export default defineComponent({
   },
   data(){
     return {
-      baseURL: process.env.VUE_APP_BASE_URL
+      baseURL: process.env.VUE_APP_BASE_URL,
+      appInfo: (process.env.VUE_APP_VERSION_INFO ? JSON.parse(process.env.VUE_APP_VERSION_INFO) : {}) as any,
+      appVersion: ""
     }
   },
   computed: {
@@ -139,6 +154,9 @@ export default defineComponent({
       showShippingOrders: 'user/showShippingOrders',
       showPackingSlip: 'user/showPackingSlip'
     })
+  },
+  mounted() {
+    this.appVersion = this.appInfo.branch ? (this.appInfo.branch + "-" + this.appInfo.revision) : this.appInfo.tag;
   },
   methods: {
     setFacility (facility: any) {
@@ -160,6 +178,9 @@ export default defineComponent({
     },
     goToOms(){
       window.open(this.instanceUrl.startsWith('http') ? this.instanceUrl.replace('api/', "") : `https://${this.instanceUrl}.hotwax.io/`, '_blank', 'noopener, noreferrer');
+    },
+    getDateTime(time: any) {
+      return DateTime.fromMillis(time).toLocaleString(DateTime.DATETIME_MED);
     }
   },
   setup () {
@@ -195,5 +216,14 @@ export default defineComponent({
   .user-profile {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  }
+  hr {
+    border-top: 1px solid var(--ion-color-medium);
+  }
+  .section-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: var(--spacer-xs) 10px 0px;
   }
 </style>
