03bf0c5b33dcf528d9071bcae507f6f4bf71cfb3
k2maan <ketuman333@gmail.com>
2023-03-21 12:20:39 +0530

    Implementd: disabling of buttons on orders page based on order update permission (#85zrthgeq)

--- src/authorization/Rules.ts
+++ src/authorization/Rules.ts
@@ -2,5 +2,6 @@ export default {
     "APP_ORDERS_VIEW": "",
     "APP_CATALOG_VIEW": "",
     "APP_ORDER_DETAIL_VIEW": "",
-    "APP_PRODUCT_DETAIL_VIEW": ""
+    "APP_PRODUCT_DETAIL_VIEW": "",
+    "APP_ORDER_UPDATE": "",
 } as any 
