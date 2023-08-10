81ba362a50bcc036e08787cb6d0670d9184aa6fd
Sanskar Soni <sonisanskar223@gmail.com>
2023-08-10 18:18:17 +0530

    Improved: indentation in orders and orderDetail view

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -20,9 +20,9 @@
         <ion-item v-if="order.shippingInstructions" color="light" lines="none">
           <ion-label class="ion-text-wrap">
             <h3>{{ $t("Handling Instructions") }}</h3>
-              <ion-text color="medium">
-                <p>{{ order.shippingInstructions }}</p>
-              </ion-text>
+            <ion-text color="medium">
+              <p>{{ order.shippingInstructions }}</p>
+            </ion-text>
           </ion-label>
         </ion-item>
         <ion-item v-if="customerEmail" lines="none">
