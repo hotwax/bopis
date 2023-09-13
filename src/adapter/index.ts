212c06961bc802c1ed4fb22d442bde90b9d87a96
Sanskar Soni <sonisanskar223@gmail.com>
2023-09-13 16:41:40 +0530

    Implemented: support for single logout

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -1,9 +1,11 @@
-import { api, client, hasError, initialise, resetConfig, updateInstanceUrl, updateToken } from '@hotwax/oms-api'
+import { api, client, getConfig, hasError, initialise, logout, resetConfig, updateInstanceUrl, updateToken } from '@hotwax/oms-api'
 
 export {
   api,
   client,
+  getConfig,
   initialise,
+  logout,
   hasError,
   resetConfig,
   updateInstanceUrl,
--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -6,7 +6,7 @@ import * as types from './mutation-types'
 import { showToast } from '@/utils'
 import i18n, { translate } from '@/i18n'
 import { Settings } from 'luxon';
-import { hasError, updateInstanceUrl, updateToken, resetConfig } from '@/adapter'
+import { hasError, logout, updateInstanceUrl, updateToken, resetConfig } from '@/adapter'
 import {
   getServerPermissionsFromRules,
   prepareAppPermissions,
@@ -92,7 +92,13 @@ const actions: ActionTree<UserState, RootState> = {
   /**
    * Logout user
    */
-  async logout ({ commit, dispatch }) {
+  async logout ({ commit, dispatch }, payload) {
+    // Calling the logout api to flag the user as logged out, only when user is authorised
+    // if the user is already unauthorised then not calling the logout api as it returns 401 again that results in a loop, thus there is no need to call logout api if the user is unauthorised
+    if(!payload?.isUserUnauthorised) {
+      await logout();
+    }
+    
     const authStore = useAuthStore()
     // TODO add any other tasks if need
     dispatch("product/clearProducts", null, { root: true })
