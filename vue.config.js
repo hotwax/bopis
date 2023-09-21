d9689e759c3369c12748c368489cc8dcf7723b0b
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-09-21 14:46:01 +0530

    Removed: i18n dependencies from the app as we have maintained it using dxp-components

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,6 @@ require("@hotwax/app-version-info")
 const path = require('path')
 
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableLegacy: true,
-      runtimeOnly: true,
-      compositionOnly: false,
-      fullInstall: true,
-      enableInSFC: true
-    }
-  },
   configureWebpack: {
     resolve: {
       alias: {
