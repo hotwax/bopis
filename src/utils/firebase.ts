75635661cbf6647683b468dede2ed27a72210e34
amansinghbais <singh11amanbais@gmail.com>
2024-07-10 19:51:22 +0530

    Improved: logic to use deviceId persisted in state

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -6,7 +6,10 @@ const storeClientRegistrationToken = async (registrationToken: string) => store.
 const addNotification = async (notification: any) => store.dispatch('user/addNotification', notification);
 
 // device ID: <DDMMYY><timestamp[6]>
-const generateDeviceId = () => (DateTime.now().toFormat('ddMMyy') + String(DateTime.now().toMillis()).slice(-6))
+const generateDeviceId = () => {
+  const deviceId = store.getters['user/getFirebaseDeviceId'];
+  return deviceId ? deviceId : (DateTime.now().toFormat('ddMMyy') + String(DateTime.now().toMillis()).slice(-6))
+}
 
 // topic name: oms-facilityId-enumId(enumCode)
 const generateTopicName = (facilityId: string, enumId: string) => {
