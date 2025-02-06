b0d620e0aa8e7b7900c8afc02ec2362c0928534d
Aashutosh soni <ashutoshsoni130@gmail.com>
2025-02-06 16:36:24 +0530

    Implemented: Step 3- atp + storeLookup data in new array, alphabetical sorting, distance based sorting, atp based toggle, improved search function.

--- src/views/OtherStoresInventoryModal.vue
+++ src/views/OtherStoresInventoryModal.vue
@@ -6,37 +6,49 @@
           <ion-icon :icon="close" />
         </ion-button>
       </ion-buttons>
-      <ion-title>{{ translate("Other stores inventory") }}</ion-title>
+      <ion-title>{{ translate("Other stores") }}</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="sortByDistance">
+          <ion-icon slot="icon-only" :icon="locationOutline"
+            :color="activeSortType === 'distance' ? 'primary' : 'medium'"></ion-icon>
+        </ion-button>
+        <ion-button @click="sortByName">
+          <ion-icon slot="icon-only" :icon="textOutline"
+            :color="activeSortType === 'name' ? 'primary' : 'medium'"></ion-icon>
+        </ion-button>
+      </ion-buttons>
     </ion-toolbar>
+    <ion-searchbar v-model="queryString" placeholder="Search store name"
+      @keyup.enter="queryString = $event.target.value; searchFacilities()" />
   </ion-header>
-  <ion-content class="ion-padding">
-    <ion-searchbar v-model="queryString" @keyup.enter="queryString = $event.target.value; searchFacilities()"/>
-    <ion-list v-if="storesInventory.length">
+  <ion-content>
+    <ion-list lines="full">
       <ion-item>
-  <ion-label>Current Facility Coordinates:</ion-label>
-  <ion-note slot="end">
-    Lat: {{ currentFacilityCoords.latitude }}, 
-    Long: {{ currentFacilityCoords.longitude }}
-  </ion-note>
-  <div v-if="nearbyStores.length">
-  <div v-for="store in nearbyStores" :key="store.identifier">
-    {{ store.storeCode }} - {{ store.dist }} km
-  </div>
-</div>
-</ion-item>
-      <ion-item v-for="storeInventory in storesInventory" :key="storeInventory.facilityName">
-        <ion-label class="ion-text-wrap">{{ storeInventory.facilityName }}</ion-label>
-        <ion-note slot="end">{{ translate('ATP', { count: storeInventory.stock}) }}</ion-note>
+        <ion-label>Hide facilities without stock</ion-label>
+        <ion-toggle @click="toggleHideEmptyStock" />
       </ion-item>
+      <div v-if="ATPMappedStoresInventory.length">
+        <ion-item v-for="inventory in ATPMappedStoresInventory" :key="inventory.storeCode">
+          <ion-label class="ion-text-wrap">
+            <p class="overline">{{ inventory.storeCode }}</p>
+            <h2>{{ inventory.storeName }}</h2>
+            <ion-note>open</ion-note>
+            <p v-if="typeof inventory.dist === 'number'">{{ inventory.dist }} miles</p>
+          </ion-label>
+          <ion-button fill="clear" slot="end">
+            <ion-note slot="end">{{ translate('ATP', { count: inventory.stock }) }}</ion-note>
+          </ion-button>
+        </ion-item>
+      </div>
+      <div v-else class="ion-text-center">
+        <p>{{ translate("No inventory details found")}}</p>
+      </div>
     </ion-list>
-    <div v-else class="ion-text-center">
-      <p>{{ translate("No inventory details found")}}</p>
-    </div>
   </ion-content>
 </template>
 
 <script lang="ts">
-import { 
+import {
   IonButtons,
   IonButton,
   IonContent,
@@ -52,14 +64,14 @@ import {
   modalController
 } from "@ionic/vue";
 import { computed, defineComponent } from "vue";
-import { close } from "ionicons/icons";
+import { close, locationOutline, textOutline } from "ionicons/icons";
 import { mapGetters } from "vuex";
 import { useStore } from "@/store";
 import { translate } from "@hotwax/dxp-components";
 
 export default defineComponent({
   name: "OtherStoresInventoryModal",
-  components: { 
+  components: {
     IonButtons,
     IonButton,
     IonContent,
@@ -71,13 +83,16 @@ export default defineComponent({
     IonNote,
     IonSearchbar,
     IonTitle,
-    IonToolbar 
+    IonToolbar
   },
-  props: ["otherStoresInventory","currentFacilityId"],
+  props: ["otherStoresInventory", "currentFacilityId"],
   data() {
-    return{
+    return {
       queryString: "",
-      storesInventory: [] as any
+      storesInventory: [] as any, // will be replaced
+      ATPMappedStoresInventory: [] as any, // will be used.
+      hideEmptyStores: false,
+      activeSortType: "name",
     }
   },
   computed: {
@@ -95,40 +110,86 @@ export default defineComponent({
   async mounted() {
     // Create a copy of otherStoresInventory on mount
     this.storesInventory = this.otherStoresInventory.slice();
-    
+
     // fetch latitude and longitude of the facility by dispatching an action
     try {
-    await this.store.dispatch("util/fetchCurrentFacilityLatLon", this.currentFacilityId);
+      await this.store.dispatch("util/fetchCurrentFacilityLatLon", this.currentFacilityId);
 
-    if (this.currentFacilityCoords?.latitude && this.currentFacilityCoords?.longitude) {
-      await this.store.dispatch("util/fetchStoreLookupByLatLon", {
-        latitude: this.currentFacilityCoords.latitude,
-        longitude: this.currentFacilityCoords.longitude
-      });
-    } else {
-      throw new Error('Invalid facility coordinates');
+      if (this.currentFacilityCoords?.latitude && this.currentFacilityCoords?.longitude) {
+        await this.store.dispatch("util/fetchStoreLookupByLatLon", {
+          latitude: this.currentFacilityCoords.latitude,
+          longitude: this.currentFacilityCoords.longitude
+        });
+        this.ATPMappedStoresInventory = this.mapStock(this.storesInventory, this.storeLookupByLatLon);
+        // Sort by name by default
+        this.sortByName();
+      } else {
+        throw new Error('Invalid facility coordinates');
+      }
+    } catch (error) {
+      throw new Error('Error fetching facility coordinates');
     }
-  } catch (error) {
-    throw new Error('Error fetching facility coordinates');
-  }
   },
   methods: {
     closeModal() {
       modalController.dismiss({ dismissed: true });
     },
-    searchFacilities(){
-      if (this.queryString.trim() !== "") {
-        this.storesInventory = this.otherStoresInventory.filter((facility: any) => facility.facilityName.toLowerCase().includes(this.queryString.toLowerCase()));
-      } else {
-        // Reset storesInventory when query is empty
-        this.storesInventory = this.otherStoresInventory.slice(); 
-      }
-    }
+    searchFacilities() {
+      // First get the full mapped inventory
+      let filteredInventory = this.mapStock(this.storesInventory, this.storeLookupByLatLon);
+  
+  // Apply search filter if query exists
+  if (this.queryString.trim() !== "") {
+    filteredInventory = filteredInventory.filter((store: any) =>
+      store.storeName.toLowerCase().includes(this.queryString.toLowerCase())
+    );
+  }
+  
+  // Apply hide empty stores filter if active
+  if (this.hideEmptyStores) {
+    filteredInventory = filteredInventory.filter((store: any) => store.stock > 0);
+  }
+  
+  this.ATPMappedStoresInventory = filteredInventory;
+}
+,
+mapStock(storesInventory: any[], storeLookupByLatLon: any[]): any[] {
+  // Skip first item (current facility) using slice(1)
+  return storeLookupByLatLon.slice(1).map((store, index) => {
+    const matchingFacility = storesInventory.find(facility => facility.facilityId === store.storeCode);
+    return {
+      ...store,
+      index: index + 1,
+      stock: matchingFacility ? matchingFacility.stock : 0
+    };
+  });
+},
+sortByDistance() {
+  this.activeSortType = 'distance';
+  this.ATPMappedStoresInventory.sort((a: any, b: any) => a.dist - b.dist);
+},
+
+sortByName() {
+  this.activeSortType = 'name';
+  this.ATPMappedStoresInventory.sort((a: any, b: any) => a.storeName.localeCompare(b.storeName));
+},
+toggleHideEmptyStock() {
+  this.hideEmptyStores = !this.hideEmptyStores;
+  if (this.hideEmptyStores) {
+    this.ATPMappedStoresInventory = this.ATPMappedStoresInventory.filter((store: any) => store.stock > 0);
+  } else {
+    // Reset to full list
+    this.ATPMappedStoresInventory = this.mapStock(this.storesInventory, this.storeLookupByLatLon);
+  }
+}
+
   },
   setup() {
     const store = useStore();
     return {
       close,
+      locationOutline,
+      textOutline,
       store,
       translate
     };
