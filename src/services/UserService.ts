d5299e26f5e71d3a7acf1036290179ebf8905c45
Yash Maheshwari <yashmaheshwari551@gmail.com>
2025-10-07 17:50:02 +0530

    Added: optional chaining to handled for undefined case

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -263,7 +263,7 @@ const getUserPermissions = async (payload: any, url: string, token: any): Promis
       return serverPermissions;
     } catch(error: any) {
       // TODO: added this to handle the flow on 401, need to remove this once we move to support of passing systemType at endpoint level
-      if(error.response.status === 401) {
+      if(error?.response?.status === 401) {
         store.dispatch("user/logout", { isUserUnauthorised: true });
         const redirectUrl = window.location.origin + '/login'
         window.location.href = `${process.env.VUE_APP_LOGIN_URL}?redirectUrl=${redirectUrl}`
