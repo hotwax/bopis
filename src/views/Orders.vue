1de014125376c8396a9bb2f3f634bde6faf932bf
Yash Maheshwari <yashmaheshwari551@gmail.com>
2026-09-09 14:38:53 +0530

    Added: support to auto refresh orders on the list page

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -143,7 +143,7 @@
 </template>
 
 <script setup lang="ts">
-import { alertController, IonBadge, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRefresher, IonRefresherContent, IonSearchbar, IonSegment, IonSegmentButton, IonTitle, IonToolbar, modalController, onIonViewWillEnter } from "@ionic/vue";
+import { alertController, IonBadge, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRefresher, IonRefresherContent, IonSearchbar, IonSegment, IonSegmentButton, IonTitle, IonToolbar, modalController, onIonViewDidLeave, onIonViewWillEnter, onIonViewWillLeave } from "@ionic/vue";
 import { onMounted, onUnmounted, ref, computed } from "vue";
 import ProductListItem from '@/components/ProductListItem.vue'
 import { mailOutline, notificationsOutline, printOutline, trailSignOutline } from "ionicons/icons";
@@ -178,6 +178,10 @@ const currentFacility = computed(() => useProductStore().getCurrentFacility);
 
 const orderStore = useOrderStore();
 
+const AUTO_REFRESH_INTERVAL = 60000;
+let autoRefreshTimer: ReturnType<typeof setInterval> | null = null;
+let isAutoRefreshing = false;
+
 const communicationEventOrderIds = computed(() => {
   return new Set((communicationEvents.value || []).map((e: any) => e.orderId));
 });
@@ -187,7 +191,9 @@ onMounted(() => {
 });
 
 onUnmounted(() => {
+  console.log('eadfdjfmkjmkmkving page')
   emitter.off("refreshPickupOrders", getPickupOrders);
+  stopAutoRefresh()
 });
 
 onIonViewWillEnter(() => {
@@ -202,7 +208,42 @@ onIonViewWillEnter(() => {
   } else {
     getCompletedOrders()
   }
+
+  startAutoRefresh()
 });
+
+onIonViewDidLeave(() => {
+  console.log('eaving page')
+  stopAutoRefresh()
+});
+
+async function autoRefreshOrders() {
+  if(isAutoRefreshing) return;
+  isAutoRefreshing = true;
+
+  try {
+    if(segmentSelected.value === 'open') {
+      await getPickupOrders(undefined, undefined, false)
+    } else if(segmentSelected.value === 'packed') {
+      await getPackedOrders(undefined, undefined, false)
+    } else {
+      await getCompletedOrders(undefined, undefined, false)
+    }
+  } finally {
+    isAutoRefreshing = false;
+  }
+}
+
+function startAutoRefresh() {
+  stopAutoRefresh()
+  autoRefreshTimer = setInterval(autoRefreshOrders, AUTO_REFRESH_INTERVAL)
+}
+
+function stopAutoRefresh() {
+  console.log('skjdnfksjdnf stop')
+  if(autoRefreshTimer) clearInterval(autoRefreshTimer)
+  autoRefreshTimer = null;
+}
 
 async function assignPicker(order: any, shipGroup: any, facilityId: any) {
   const assignPickerModal = await modalController.create({
@@ -249,46 +290,47 @@ async function refreshOrders(event: any) {
 }
 
 async function viewOrder(orderData: any, shipGroupSeqId: any, orderType: any) {
+  stopAutoRefresh()
   orderData['orderType'] = orderType
   await useOrderStore().updateCurrent({ order: orderData })
   router.push({ path: `/orderdetail/${orderType}/${orderData.orderId}/${shipGroupSeqId}` })
 }
 
-async function getPickupOrders(vSize?: any, vIndex?: any) {
+async function getPickupOrders(vSize?: any, vIndex?: any, showLoader = true) {
   const viewSize = vSize ? vSize : import.meta.env.VITE_VIEW_SIZE;
   const viewIndex = vIndex ? vIndex : 0;
-  emitter.emit("presentLoader")
+  if (showLoader) emitter.emit("presentLoader")
   useOrderStore().clearOrders();
   await useOrderStore().fetchOpenOrders({ viewSize, viewIndex, queryString: queryString.value, facilityId: (currentFacility.value as any)?.facilityId });
   if(useProductStore().isProductStoreSettingEnabled('SHOW_SHIPPING_ORDERS')) {
     await useOrderStore().fetchOpenOrders({ viewSize, viewIndex, queryString: queryString.value, facilityId: (currentFacility.value as any)?.facilityId, showShippingOrders: true });
   }
-  emitter.emit("dismissLoader")
+  if (showLoader) emitter.emit("dismissLoader")
 }
 
-async function getPackedOrders(vSize?: any, vIndex?: any) {
+async function getPackedOrders(vSize?: any, vIndex?: any, showLoader = true) {
   const viewSize = vSize ? vSize : import.meta.env.VITE_VIEW_SIZE;
   const viewIndex = vIndex ? vIndex : 0;
-  emitter.emit("presentLoader")
+  if (showLoader) emitter.emit("presentLoader")
   useOrderStore().clearOrders();
   await useOrderStore().fetchPackedOrders({ viewSize, viewIndex, queryString: queryString.value, facilityId: (currentFacility.value as any)?.facilityId, shipmentMethodTypeIds: "STOREPICKUP" });
   if(useProductStore().isProductStoreSettingEnabled('SHOW_SHIPPING_ORDERS')) {
     await useOrderStore().fetchPackedOrders({ viewSize, viewIndex, queryString: queryString.value, facilityId: (currentFacility.value as any)?.facilityId });
   }
-  emitter.emit("dismissLoader")
+  if (showLoader) emitter.emit("dismissLoader")
 }
 
-async function getCompletedOrders(vSize?: any, vIndex?: any) {
+async function getCompletedOrders(vSize?: any, vIndex?: any, showLoader = true) {
   const viewSize = vSize ? vSize : import.meta.env.VITE_VIEW_SIZE;
   const viewIndex = vIndex ? vIndex : 0;
-  emitter.emit("presentLoader")
+  if (showLoader) emitter.emit("presentLoader")
   useOrderStore().clearOrders();
   await useOrderStore().fetchCompletedOrders({ viewSize, viewIndex, queryString: queryString.value, facilityId: (currentFacility.value as any)?.facilityId, shipmentMethodTypeIds: "STOREPICKUP" });
   if(useProductStore().isProductStoreSettingEnabled('SHOW_SHIPPING_ORDERS')) {
     await useOrderStore().fetchCompletedOrders({ viewSize, viewIndex, queryString: queryString.value, facilityId: (currentFacility.value as any)?.facilityId });
   }
   if (useUserStore().hasPermission(Actions.APP_PROOF_OF_DELIVERY_PREF_UPDATE)) await useOrderStore().fetchCommunicationEvents({ orders: completedOrders.value });
-  emitter.emit("dismissLoader")
+  if (showLoader) emitter.emit("dismissLoader")
 }
 
 async function readyForPickup(orderData: any, shipGroup: any) {
